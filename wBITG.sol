437175f63af95e27e391238f20429a991ee83731
ferdreyfus <fdreyfus@gmail.com>
2020-08-16 12:29:50 -0500

    Update wBITG.sol

--- wBITG.sol
+++ wBITG.sol
@@ -5,7 +5,8 @@ pragma solidity ^0.6.12;
 // wBITG is a BITG collateralized ERC20 Version of BitGreen Foundation's BITG. 
 // Balance in this contract, must match collateral address. You may verify balance Here: "https://explorer.bitg.org/address/GWhA3Ksf3JEHh3UvXKhbMALo1pW4328xVR"
 // please, visit BITG.org for more information, and confirmation of wBITG Contract Address.
-// Only interact with official 
+// Only interact with official BitGreen Foundation Contract, confirm validity through our Website, BITG.Org, Telegram or Discord channel. 
+// Updates and announcements will be shown on official wBITG section of website. 
 
 // Library 1 - Enumerables Set
 
